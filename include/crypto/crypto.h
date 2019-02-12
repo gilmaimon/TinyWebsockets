24924ea7b61db258b50c663068861fa1c3011dff
Gil Maimon <gilm2121@gmail.com>
2019-02-12 23:04:01 +0200

    added crypto helpers to help with websockets handshake

---
+++ include/crypto/crypto.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "common.h"
+#include "base64.h"
+#include "sha1.h"
+
+namespace crypto {
+    String base64Encode(String data) {
+      return internals::base64_encode((const unsigned char *) data.c_str(), data.size());
+    }
+    String base64Encode(uint8_t* data, size_t len) {
+      return internals::base64_encode((const unsigned char *) data, len);
+    }
+    
+    String base64Decode(String data) {
+      return internals::base64_decode(data);
+    }
+
+    String sha1(String data) {
+        SHA1 checksum;
+        checksum.update(data);
+        return checksum.final();
+    }    
+}
--- src/websockets_client.cpp
+++ src/websockets_client.cpp
@@ -3,25 +3,55 @@
 #include "websockets/data_frame.h"
 #include "websockets/message.h"
 #include "websockets/websockets_client.h"
+#include "crypto/crypto.h"
 #include <memory.h>
 
 WebSocketsClient::WebSocketsClient(TcpClient* client) : _client(client), WebSocketsEndpoint(*client) {
     // Empty
 }
 
+struct HandshakeRequestResult {
+    String requestStr;
+    String expectedAcceptKey;
+};
+HandshakeRequestResult generateHandshake(String uri) {
+    String randomBytes = "1234567890123456";
+
+    String handshake = "GET " + uri + " HTTP/1.1\r\n";
+    handshake += "Upgrade: websocket\r\n";
+    handshake += "Connection: Upgrade\r\n";
+    handshake += "Sec-WebSocket-Key: " + crypto::base64Encode(randomBytes) + "\r\n";
+    handshake += "Sec-WebSocket-Version: 13\r\n";
+    handshake += "\r\n";
+
+    String expectedAccept = crypto::base64Encode(crypto::sha1(
+        crypto::base64Encode(randomBytes) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+    ));
+
+    HandshakeRequestResult result;
+    result.requestStr = handshake;
+    result.expectedAcceptKey = expectedAccept;
+    return std::move(result);
+}
+
+bool isWhitespace(char ch) {
+    return ch == ' ' || ch == '\t';
+}
+
 bool WebSocketsClient::connect(String host, int port) {
     bool connected = this->_client->connect(host, port);
     if (!connected) return false;
 
-    // TODO fix hardcoded Key
-    String handshake = "GET / HTTP/1.1\r\n";
-    handshake += "Upgrade: websocket\r\n";
-    handshake += "Connection: Upgrade\r\n";
-    handshake += "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n";
-    handshake += "Sec-WebSocket-Version: 13\r\n";
-    handshake += "\r\n";
+    auto handshake = generateHandshake("/");
+    this->_client->send(handshake.requestStr);
 
-    this->_client->send(handshake);
+
+    auto head = this->_client->readLine();
+    if(head != "HTTP/1.1 101 Switching Protocols") {
+        //TODO indicate Error!!
+        return false;
+    }
+
     // TODO don't ignore server's response
     while (true) {
         auto line = this->_client->readLine();
