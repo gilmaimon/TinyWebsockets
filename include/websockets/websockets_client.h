8d9432820c3cf225e190f25a64a536bc5835e92f
Gil Maimon <gilm2121@gmail.com>
2019-02-12 12:59:59 +0200

    added private handlers for control packets (close, ping, pong)

--- include/websockets/websockets_client.h
+++ include/websockets/websockets_client.h
@@ -14,11 +14,17 @@ public:
 	void setMessageHandler(MessageCallback callback);
 	void poll();
 	bool available();
+
 	void send(String data);
+	void sendBinary(String data);
 
 	~WebSocketsClient();
 
 private:
 	TcpClient* _client;
 	MessageCallback _callback;
+
+	void _handlePing(WebsocketsMessage);
+	void _handlePong(WebsocketsMessage);
+	void _handleClose(WebsocketsMessage);
 };
--- src/websockets_client.cpp
+++ src/websockets_client.cpp
@@ -35,18 +35,47 @@ void WebSocketsClient::setMessageHandler(MessageCallback callback) {
 }
 
 void WebSocketsClient::poll() {
-    if(WebSocketsEndpoint::poll()) {
-        auto frame = WebSocketsEndpoint::recv();
-        this->_callback(WebsocketsMessage::CreateFromFrame(frame));
+    if(!WebSocketsEndpoint::poll()) {
+        return;
+    }
+
+    auto frame = WebSocketsEndpoint::recv();
+    
+    auto msg = WebsocketsMessage::CreateFromFrame(frame);
+    if(msg.isBinary() || msg.isText()) {
+        this->_callback(std::move(msg));
+    } else if(msg.type() == MessageType::Ping) {
+        _handlePing(std::move(msg));
+    } else if(msg.type() == MessageType::Pong) {
+        _handlePong(std::move(msg));
+    } else if(msg.type() == MessageType::Close) {
+        _handleClose(std::move(msg));
     }
 }
 
 void WebSocketsClient::send(String data) {
-    WebSocketsEndpoint::send(data, 0x1);
+    WebSocketsEndpoint::send(data, MessageType::Text);
+}
+
+void WebSocketsClient::sendBinary(String data) {
+    WebSocketsEndpoint::send(data, MessageType::Binary);
 }
 
 bool WebSocketsClient::available() {
     return this->_client->available();
+}
+
+
+void WebSocketsClient::_handlePing(WebsocketsMessage) {
+    // TODO handle ping
+}
+
+void WebSocketsClient::_handlePong(WebsocketsMessage) {
+    // TODO handle pong
+}
+
+void WebSocketsClient::_handleClose(WebsocketsMessage) {
+    //TODO handle close
 }
 
 WebSocketsClient::~WebSocketsClient() {
