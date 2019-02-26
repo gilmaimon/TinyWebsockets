ad11bc9cb4dd170705cf49b6f109b3a1ecc66c33
Gil Maimon <gilm2121@gmail.com>
2019-02-27 00:21:29 +0200

    added a server test. uses pthreads and does not work on windows with mingw

---
+++ tests/src/basic_server.cpp
@@ -0,0 +1,40 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <tiny_websockets/client.hpp>
+#include <tiny_websockets/server.hpp>
+
+#include <iostream>
+#include <thread>
+
+using namespace websockets;
+
+void test_server() {
+    WebsocketsServer server;
+    server.listen(8181);
+
+    auto connectedClient = server.accept();
+    REQUIRE( connectedClient.available() );
+
+    connectedClient.send("Hello Client");
+    connectedClient.close();
+}
+
+TEST_CASE( "Testing Server creation" ) {
+
+    std::thread serverThread(test_server);
+
+    WebsocketsClient client;
+    
+    std::cout << "Doing Connect" << std::endl;
+    REQUIRE( client.connect("localhost", 8181, "/") );
+    std::cout << "After Connect" << std::endl;
+
+    auto message = client.readBlocking();
+
+    REQUIRE( message.type() == MessageType::Text );
+    REQUIRE( message.data() == "Hello Client" );
+    
+    client.close();
+    serverThread.join();
+}
