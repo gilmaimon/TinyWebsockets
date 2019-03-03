83495cfe4db0aed39a0a4f62056e9263ad128fe6
Gil Maimon <gilm2121@gmail.com>
2019-03-03 21:51:56 +0200

    TEST: added a ping the middle of streaming to make sure it does not interupt with recving and sending the fragments

--- tests/src/fragments_echo_test.cpp
+++ tests/src/fragments_echo_test.cpp
@@ -8,52 +8,60 @@ using namespace websockets;
 TEST_CASE( "Testing sending fragmented messages - Receiving the messages with Aggregate policy" ) {
   WebsocketsClient client;
   client.setFragmentsPolicy(FragmentsPolicy_Aggregate);
-    REQUIRE( client.connect("ws://echo.websocket.org") == true );
-    volatile bool done = false;
+  REQUIRE( client.connect("ws://echo.websocket.org") == true );
+  volatile bool done = false;
 
-    client.onMessage([&done](WebsocketsClient& client, auto message){
-        REQUIRE( message.isContinuation() == false );
-        std::cout << (message.isContinuation()? "(continuation) " : "") << "Message Got: " << message.data() << std::endl;
-        REQUIRE( message.data() == "Msg: Hello World." );
-        done = true;
-    });
+  client.onMessage([&done](WebsocketsClient&, auto message){
+      REQUIRE( message.isContinuation() == false );
+      std::cout << (message.isContinuation()? "(continuation) " : "") << "Message Got: " << message.data() << std::endl;
+      REQUIRE( message.data() == "Msg: Hello World." );
+      done = true;
+  });
 
-    // Stream "Msg: Hello World."
-    client.stream("Msg: ");
-    client.send("Hello World");
-    client.end(".");
+  // Stream "Msg: Hello World."
+  client.stream("Msg: ");
 
-    REQUIRE( client.available() == true);
-    while(!done) client.poll();
+  // send a ping in the middle of the stream, should not change anything
+  client.ping("PingData");
+
+  client.send("Hello World");
+  client.end(".");
+
+  REQUIRE( client.available() == true);
+  while(!done) client.poll();
 }
 
 TEST_CASE( "Testing sending fragmented messages - Receiving the messages with Notify policy" ) {
   WebsocketsClient client;
   client.setFragmentsPolicy(FragmentsPolicy_Notify);
-    REQUIRE( client.connect("ws://echo.websocket.org") == true );
-    volatile int recvdCount = 0;
+  REQUIRE( client.connect("ws://echo.websocket.org") == true );
+  volatile int recvdCount = 0;
 
-    client.onMessage([&recvdCount](WebsocketsClient& client, auto message){
-        std::cout << (message.isContinuation()? "(continuation) " : "") << "Message Got: " << message.data() << std::endl;
-        REQUIRE( message.isContinuation() == true );
-        if(recvdCount == 0) {
-          REQUIRE( message.data() == "Msg: " );
-        } else if(recvdCount == 1) {
-          REQUIRE( message.data() == "Hello World" );
-        } else if(recvdCount == 2) {
-          REQUIRE( message.data() == "." );
-        } else if(recvdCount == 3) {
-          REQUIRE( "THIS STATE SHOULD NOT OCCUR" == nullptr);
-          recvdCount = 3;
-        }
-        ++recvdCount;
-    });
+  client.onMessage([&recvdCount](WebsocketsClient&, auto message){
+      std::cout << (message.isContinuation()? "(continuation) " : "") << "Message Got: " << message.data() << std::endl;
+      REQUIRE( message.isContinuation() == true );
+      if(recvdCount == 0) {
+        REQUIRE( message.data() == "Msg: " );
+      } else if(recvdCount == 1) {
+        REQUIRE( message.data() == "Hello World" );
+      } else if(recvdCount == 2) {
+        REQUIRE( message.data() == "." );
+      } else if(recvdCount == 3) {
+        REQUIRE( "THIS STATE SHOULD NOT OCCUR" == nullptr);
+        recvdCount = 3;
+      }
+      ++recvdCount;
+  });
 
-    // Stream "Msg: Hello World."
-    client.stream("Msg: ");
-    client.send("Hello World");
-    client.end(".");
+  // Stream "Msg: Hello World."
+  client.stream("Msg: ");
 
-    REQUIRE( client.available() == true);
-    while(recvdCount < 3) client.poll();
+  // send a ping in the middle of the stream, should not change anything
+  client.ping("PingData");
+  
+  client.send("Hello World");
+  client.end(".");
+
+  REQUIRE( client.available() == true);
+  while(recvdCount < 3) client.poll();
 }
