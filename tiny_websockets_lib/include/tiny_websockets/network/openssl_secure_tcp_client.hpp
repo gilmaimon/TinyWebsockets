f9d30a67b3f620f52163c7ce9f918b07fc94a6bf
Gil Maimon <gilm2121@gmail.com>
2019-03-22 14:25:53 +0200

    FIX: fixed openssl runtime and compile time error on linux (no TLS_..)

--- tiny_websockets_lib/include/tiny_websockets/network/openssl_secure_tcp_client.hpp
+++ tiny_websockets_lib/include/tiny_websockets/network/openssl_secure_tcp_client.hpp
@@ -12,15 +12,21 @@ namespace websockets { namespace network { namespace internals {
   SSL_CTX *InitSSL_CTX(void) {
     ERR_load_crypto_strings();
     SSL_load_error_strings();
-    
+
     #if OPENSSL_VERSION_NUMBER < 0x10100000L
     SSL_library_init();
     #else
     OPENSSL_init_ssl(0, NULL);
     #endif
 
+    #if defined(LWS_HAVE_TLS_CLIENT_METHOD)
+	const SSL_METHOD *method = TLS_client_method();
+    #elif defined(LWS_HAVE_TLSV1_2_CLIENT_METHOD)
+	const SSL_METHOD *method = TLSv1_2_client_method();
+    #else
+	const SSL_METHOD *method = SSLv23_client_method();
+    #endif
 
-    const SSL_METHOD *method = TLS_client_method(); /* Create new client-method instance */
     SSL_CTX *ctx = SSL_CTX_new(method);
 
     if (ctx == nullptr)
