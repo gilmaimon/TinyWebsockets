2777e90353243b897d4f4f6ebf49ec1105fcd6e1
Gil Maimon <gilm2121@gmail.com>
2019-06-08 13:31:22 +0300

    Added a test for close connection events

---
+++ tests/src/close_event_test.cpp
@@ -0,0 +1,62 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <tiny_websockets/client.hpp>
+
+using namespace websockets;
+
+#define MSG_DATA std::string("Hi, This is data sent")
+
+TEST_CASE( "Testing - Event callback for `close` should be called when user closes the connection (no-reason)" ) {
+    WebsocketsClient client;
+    volatile int closeCalled = 0;
+    volatile int openCalled = 0;
+
+    client.onEvent([&openCalled, &closeCalled](WebsocketsClient& client, WebsocketsEvent event, auto data) {
+        if(event == WebsocketsEvent::ConnectionClosed) {
+            REQUIRE( client.getCloseReason() == CloseReason_NormalClosure );
+            ++closeCalled;
+        } else if(event == WebsocketsEvent::ConnectionOpened) {
+            REQUIRE( closeCalled == 0 );
+            ++openCalled;
+        } else {
+            REQUIRE( false == true ); // Should never happen in this test
+        }
+    });
+
+    REQUIRE( client.connect("localhost", 8080, "/") == true );
+    REQUIRE( client.available() == true );
+    client.close();
+    
+    REQUIRE( client.available() == false );
+    REQUIRE( client.getCloseReason() == CloseReason_NormalClosure );
+    REQUIRE( closeCalled == 1 );
+    REQUIRE( openCalled == 1 );
+}
+
+TEST_CASE( "Testing - Event callback for `close` should be called when user closes the connection (specified-reason)" ) {
+    WebsocketsClient client;
+    volatile int closeCalled = 0;
+    volatile int openCalled = 0;
+
+    client.onEvent([&openCalled, &closeCalled](WebsocketsClient& client, WebsocketsEvent event, auto data) {
+        if(event == WebsocketsEvent::ConnectionClosed) {
+            REQUIRE( client.getCloseReason() == CloseReason_UnsupportedData );
+            ++closeCalled;
+        } else if(event == WebsocketsEvent::ConnectionOpened) {
+            REQUIRE( closeCalled == 0 );
+            ++openCalled;
+        } else {
+            REQUIRE( false == true ); // Should never happen in this test
+        }
+    });
+
+    REQUIRE( client.connect("localhost", 8080, "/") == true );
+    REQUIRE( client.available() == true );
+    client.close(CloseReason_UnsupportedData);
+    
+    REQUIRE( client.available() == false );
+    REQUIRE( client.getCloseReason() == CloseReason_UnsupportedData );
+    REQUIRE( closeCalled == 1 );
+    REQUIRE( openCalled == 1 );
+}
